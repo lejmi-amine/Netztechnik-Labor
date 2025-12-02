a2f5cc6e84b577262a4193eb76b633340bfe97f6
Amine Lejmi <60818050+lejmi-amine@users.noreply.github.com>
2025-12-02 20:18:39 +0100

    Delete src directory

--- src/sptree.h
+++
@@ -1,8 +0,0 @@
-#ifndef SPTREE_H
-#define SPTREE_H
-
-void sptree_step(int index);
-void run_simulation(int min_iterations);
-void print_spanning_tree();
-
-#endif
--- src/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-int isValid(const char *str);
-int checkline(const char *line);
-void trim(char *str);
-
-#endif
